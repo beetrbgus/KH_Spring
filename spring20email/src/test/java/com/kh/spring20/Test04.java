3113cfa16d1ee2ac88344c74a756cbc6ba8359e7
beetrbgus <devbeetrb0214@gmail.com>
2021-12-14 14:11:08 +0900

    RandomUtil 위치 수정에 따른 import 변경

--- spring20email/src/test/java/com/kh/spring20/Test04.java
+++ spring20email/src/test/java/com/kh/spring20/Test04.java
@@ -3,14 +3,11 @@ package com.kh.spring20;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.kh.spring20.component.RandomUtil;
-import com.kh.spring20.service.EmailService;
+import com.kh.spring20.util.RandomUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
