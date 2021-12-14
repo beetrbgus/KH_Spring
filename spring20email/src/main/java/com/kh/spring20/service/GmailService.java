e5b4470874dd2ecf5bb3a984bec13cb5f8e378fa
beetrbgus <devbeetrb0214@gmail.com>
2021-12-14 13:37:19 +0900

    이메일 인증 시스템 및 스케쥴러

--- spring20email/src/main/java/com/kh/spring20/service/GmailService.java
+++ spring20email/src/main/java/com/kh/spring20/service/GmailService.java
@@ -9,28 +9,40 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import com.kh.spring20.component.RandomUtil;
-import com.kh.spring20.component.RandomUtilImpl;
+import com.kh.spring20.entity.CertificationDto;
+import com.kh.spring20.repository.CertificationDao;
+import com.kh.spring20.util.RandomUtil;
 
 @Service
 public class GmailService implements EmailService{
 	
 	@Autowired
 	private JavaMailSender sender;
-	@Autowired
-	private RandomUtil randomNumber; 
 	
+	@Autowired
+	private RandomUtil randomUtil;
+	
+	@Autowired
+	private CertificationDao certificationDao;
+
 	@Override
 	public void sendCertificationNumber(String to) {
+		//랜덤번호 생성 부분
+		String number = randomUtil.generateRandomNumber(6);
 		
-		String number = randomNumber.generateRandomNumber(6);
+		//실제 이메일 발송 부분
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(to);
-		message.setSubject("[쿠쿠루삥뽕] 인증번호 메일입니다.");
-		message.setText(" 인증번호 : "+number);
+		message.setSubject("[KH정보교육원] 인증번호 메일입니다");
+		message.setText("인증번호 : " + number);
 		sender.send(message);
 		
+		//데이터베이스 등록 부분
+		CertificationDto certificationDto = new CertificationDto();
+		certificationDto.setEmail(to);
+		certificationDto.setSerial(number);
+		
+		certificationDao.insert(certificationDto);
 	}
-	
 	
 }
