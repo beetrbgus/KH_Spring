82ae5ad2b0f2d2f5f4f0eacb5d7a20ea20c580bb
beetrbgus <devbeetrb0214@gmail.com>
2021-12-22 13:44:32 +0900

    자바로 설정하는 법

---
+++ spring28configuration/src/main/java/com/kh/spring28/Spring28configurationApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring28configurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring28configurationApplication.class, args);
+	}
+
+}
---
+++ spring28configuration/src/test/java/com/kh/spring28/Spring28configurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring28configurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
